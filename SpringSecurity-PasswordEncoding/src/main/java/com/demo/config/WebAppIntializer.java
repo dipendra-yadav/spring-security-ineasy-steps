d2fd4b3dbb88978635cfb4b8f6111ac19020ff24
dipendra-yadav <dipendra.eng@gmail.com>
2020-03-06 16:44:34 +0530

    commit.

---
+++ SpringSecurity-PasswordEncoding/src/main/java/com/demo/config/WebAppIntializer.java
@@ -0,0 +1,24 @@
+package com.demo.config;
+
+import org.springframework.web.servlet.support.*;
+
+public class WebAppIntializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
